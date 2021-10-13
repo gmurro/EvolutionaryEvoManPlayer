879b447a22508678f57d188433f5645b829df009
gmurro <g.murro23@gmail.com>
2021-10-13 12:28:36 +0200

    hyperparameter_tuning.py ready for the tuning

--- ea1/hyperparameter_tuning.py
+++ ea1/hyperparameter_tuning.py
@@ -4,6 +4,7 @@ Uses HyperOpt, a library for hyperparameter tuning.
 The GeneticOptimizer object will take care of the evolution and just return the best individual's fitness.
 """
 import os
+import csv
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 os.environ["SDL_VIDEODRIVER"] = "dummy"
@@ -13,27 +14,47 @@ from singlelayer_controller import PlayerController
 from hyperopt import hp, fmin, tpe
 from hyperopt import SparkTrials
 
+TUNING_DIR = "hyperparameter_tuning"
+
 # fixed parameters
 ENEMY = [2, 5, 8]
 LAYER_NODES = [20, 10, 5]
 GENERATIONS = 20
 LAMBDA = 5
 MUTATION_STEP_SIZE = 1.
+N_ISLANDS = 10
+
+# file name of the csv where store intermediate results
+file_path = os.path.join(TUNING_DIR, "logbook_tuning.csv")
+
+
+def write_header(file_path):
+    """
+    Write the header for the csv file
+    """
+    os.makedirs(TUNING_DIR, exist_ok=True)
+    fields = ['pop_size', 'cx_prob', 'cx_alpha', 'mut_prob', 'mut_indpb', 'tournsize', 'migration_size', 'fitness_best',
+              'gain_best']
+    with open(file_path, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(fields)
 
 
 def test_hyperparameters_vector(args):
     """
     Tests an hyperparameter vector with GeneticOptimizer
     """
+
     print(f"Now trying combination {args}")
-    game_runner = GameRunner(PlayerController(LAYER_NODES), enemies=ENEMY, headless=True)
+    game_runner = GameRunner(PlayerController(LAYER_NODES[1]), enemies=ENEMY, headless=True)
     optimizer = GeneticOptimizer(
         enemies=ENEMY,
         layer_nodes=LAYER_NODES,
+        n_islands=N_ISLANDS,
         population_size=int(args["population_size"]),
         game_runner=game_runner,
         generations=GENERATIONS,
-        lambda_offspring = LAMBDA,
+        lambda_offspring=LAMBDA,
         cx_probability=round(args["cx_probability"], 2),
         cx_alpha=round(args["cx_alpha"], 2),
         mut_probability=round(args["mut_probability"], 2),
@@ -45,20 +66,29 @@ def test_hyperparameters_vector(args):
         parallel=True,
     )
     best_individual = optimizer.evolve()
-    print(f"TRIAL with:")
-    print(f"pop_size={int(args['population_size'])}, cx_prob={round(args['cx_probability'], 2)}, cx_alpha={round(args['cx_alpha'], 2)}, "
-          f"mut_prob={round(args['mut_probability'], 2)}, mut_indpb={round(args['mutation_indpb'], 2)}, tournsize={int(args['tournsize'])},"
-          f"migration_interval={int(args['migration_interval'])}, migration_size={int(args['migration_size'])}")
-    print(f"fitness_best={best_individual['fitness']}")
-    print(f"gain_best={best_individual['individual_gain']}")
-    return -best_individual["fitness"]
+
+    # write the combination of hyperparameters used in this trial to this file
+    row = [int(args['population_size']), round(args['cx_probability'], 2), round(args['cx_alpha'], 2),
+           round(args['mut_probability'], 2), round(args['mutation_indpb'], 2), int(args['tournsize']),
+           int(args['migration_interval']), int(args['migration_size']), best_individual['fitness'],
+           best_individual['individual_gain']
+           ]
+    with open(file_path, 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
+
+    return -best_individual["individual_gain"]
 
 
+# initialize the headers in the csv file
+write_header(file_path)
+
+# define the search space across the hyperparameters
 space = hp.choice(
     "GA",
     [
         {
-            "population_size": hp.quniform("population_size", 5, 10, 1),
+            "population_size": hp.quniform("population_size", 6, 11, 1),
             "cx_probability": hp.uniform("cx_probability", 0.4, 1),
             "cx_alpha": hp.uniform("cx_alpha", 0, 1),
             "mut_probability": hp.uniform("mut_probability", 0, 0.6),
@@ -77,5 +107,6 @@ best = fmin(
     algo=tpe.suggest,
     max_evals=72,
 )
+
 print("\nThe best combination of hyperparameters is:")
 print(best)
